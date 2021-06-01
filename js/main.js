5dd1307b3155661f1676a168fa7dbaa65a748830
Sergey <podshiblov2015@yandex.ru>
2021-06-01 21:54:19 +0300

    правки js

--- js/main.js
+++ js/main.js
@@ -3,28 +3,24 @@
 //Разобрался и повторил из источника, придумал как функция должна вести себя при до < от.
 
 function getRandomNumber(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
+	if (max <= min ) {
+	  return -1;
+	}
 
-  if (min < 0 || max < 0) {
-    return 'You cannot return random numbers. The maximum number must be greater than the minimum.';
-  }
+	const minNum = Math.ceil(min);
+	const maxNum = Math.floor(max);
 
-  if (max < min || max === min) {
-    return 'You cannot return random numbers. The maximum number, it cannot be less or identical to min number.';
-  }
-
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+	return Math.floor(Math.random() * (maxNum - minNum + 1)) + minNum;
+ }
 
 getRandomNumber(1, 9);
 
 //Сделал сам.
 
-function checkLengthString(checkString,maxLength) {
-  if (checkString <= maxLength) {
-    return true;
-  }
-}
+function checkLengthString(thisString,maxLength) {
+	if (checkString.length <= maxLength) {
+	  return true;
+	}
+ }
 
-checkLengthString(10,140);
+ checkLengthString('test',140);
